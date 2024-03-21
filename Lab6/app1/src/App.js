7b3f7c3ebcc47c24dd28a079f9e4b766a4b6a7b1
Mohammed Ockba <7790976+mockba1501@users.noreply.github.com>
2024-03-21 20:10:05 +0200

    Lab6 React Lab Part2
    Event Handling
    UseState
    Props

---
+++ Lab6/app1/src/App.js
@@ -0,0 +1,60 @@
+import {useState} from 'react';
+import logo from './logo.svg';
+import About from './About';
+import './App.css';
+function TestingComponent({mohammed, ClickFunction})
+{
+
+  return (
+    <button onClick={ClickFunction}>Clicked Me {mohammed} times</button>
+  );
+}
+
+function SignUp()
+{
+  return (
+    <button>Sign up</button>
+  );
+}
+
+function LogIn()
+{
+  return (
+    <button>Log In</button>
+  );
+}
+
+let isNewUser = false;
+let content;
+/*
+if(isNewUser)
+{
+  content = <SignUp/>;
+}
+else
+{
+  content = <LogIn/>;
+}
+*/
+function App() {
+  const [count,setCount] = useState(0);
+
+  function handleClick()
+  {
+    setCount(count + 1);
+    //alert('the button has been clicked');
+  }
+
+  return (
+    <>
+    <h1>Hello to our first app</h1>
+    <p>This is a test for paragraph</p>
+    <TestingComponent mohammed={count} ClickFunction={handleClick}/>
+    <TestingComponent mohammed={count} ClickFunction={handleClick}/>
+    <About />
+    {isNewUser? <SignUp/>: <LogIn/>}
+    </>
+  );
+}
+
+export default App;
