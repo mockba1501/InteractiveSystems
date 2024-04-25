6fdb821115660892eb7f91150f84884bea24abc7
Mohammed Ockba <7790976+mockba1501@users.noreply.github.com>
2024-04-25 19:59:46 +0300

    Movie React App Class

---
+++ Lab7/app1/src/App.js
@@ -0,0 +1,32 @@
+import {useState, useEffect} from 'react';
+import './App.css';
+import APIKey from './config';
+import Movie from './Components/Movie';
+
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=';
+
+function App() {
+  const [movieResults, setMovieResults] = useState([]); 
+  //const movieResults = ['age',4512,true, "aa"];
+
+  useEffect( ()=> {
+    fetch(APIURL + APIKey)
+    .then( (resp) => resp.json() )
+    .then( (data) => {
+        console.log(data);
+        setMovieResults(data.results);
+        }
+        )
+  }
+  ,[])
+
+  return (
+    <>
+    {movieResults.map( (element,index) => (<Movie key={index}/>)
+
+    )}
+    </>
+  );
+}
+
+export default App;
---
+++ Lab7/app1/src/Components/Movie.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Movie()
+{
+    return (
+        <div>This is a Movie Component</div>
+    );
+}
+
+export default Movie;
